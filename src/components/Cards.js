13a82893b8c399d482ab58205d321992e8106de1
yair <yairmasterlol@gmail.com>
2023-02-15 16:03:09 -0600

    15-feb-23 / 4:43

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -8,73 +8,32 @@ import { Pagination, EffectCards, Mousewheel, Keyboard } from "swiper";
 import wordSound from "../media/cards/audio.png";
 import iconAdd from "../media/add.png";
 import Loader from "./Loader";
-import { Link, NavLink, useParams } from "react-router-dom";
+import { Link, NavLink, useNavigate, useParams } from "react-router-dom";
 import AuthContext from "../context/AuthContext";
 import FormCard from "./FormCard";
 import PayPal from "./PayPal";
 import { helpHttp } from "../helpers/helpHttp";
-import Modal from "react-modal";
-import OutsideClickHandler from "react-outside-click-handler";
-import next from "../media/next3.png";
 import mySite from "./Domain";
-import { isMobile } from "react-device-detect";
 import axios from "axios";
 // import Phrases from "./Phrases";
 import NewMenu from "./NewMenu";
-// import CardTuto from "./CardTuto";
+// import Quiz from "../components/Quiz";
 
+// import CardTuto from "./CardTuto";
 let url = "";
 const urlImageCard = "https://res.cloudinary.com/tolumaster/image/upload/v1/";
-const API_KEY = "60c9c98bb968461eb176f4d26b790bf2",
-  language = "en-us",
-  speed = 0,
-  voice = "female";
 
 const Cards = () => {
   let { user } = useContext(AuthContext);
-  // console.log(user.user_id);
-
-  let urlIncreaseScore = `${mySite}increase/${user.user_id}`;
+  const navigate = useNavigate();
   const audioRef = useRef();
-
   const paramsUrl = useParams();
   const [audio, setAudio] = useState();
-
   const [isActive, setIsActive] = useState(true);
   const [modalIsOpen, setModalIsOpen] = useState(false);
   const [isPremium, setIsPremium] = useState(false);
   const [cards, setCards] = useState([]);
-  const [answerSelected, setAnswerSelected] = useState();
-  const [cardPicked, setCardPicked] = useState();
-  const [modalQuestion, setModalQuestion] = useState(false);
-  const [result, setResult] = useState();
-  const [answers, setAnswers] = useState([]);
-  const [isSent, setIsSent] = useState(false);
-  const [mode, setMode] = useState(true);
-  const [checked, setChecked] = useState();
   const [loader, setLoader] = useState(false);
-
-  const customStyles = {
-    content: {
-      // color: "black",
-      width: isMobile ? "84vw" : "320px",
-      height: "440px",
-      backgroundColor: mode ? "white" : "black",
-      top: isMobile ? "50%" : "40%",
-      left: "50%",
-      transform: "translate(-50%, -50%)",
-      border: "solid 1px #270969",
-      // backgroundColor: "#00000000",
-      outline: "none",
-      transition: "1s ease-in-out",
-      borderRadius: "10px",
-      overflow: "hidden",
-      fontFamily: "Mulish, sans-serif",
-
-      // padding: "0",
-    },
-    overlay: { zIndex: 1000, backgroundColor: "rgba(0, 0, 0, 0.9)" },
-  };
 
   const fetchAPi = async () => {
     setLoader(true);
@@ -96,97 +55,14 @@ const Cards = () => {
         setIsPremium(res.user.premium);
       });
   };
-
   useEffect(() => {
     fetchAPi();
     getUserData();
   }, []);
 
-  const handleDisplay = (e) => {
-    isActive ? setIsActive(false) : setIsActive(true);
-  };
-  const hideQuestion = () => {
-    setModalQuestion(false);
-    setResult();
-    setIsSent(false);
-  };
+  const handleDisplay = () => setIsActive(!isActive);
 
-  const openModal = (e) => {
-    setModalIsOpen(!modalIsOpen);
-  };
-
-  function differentRandom() {
-    var randoms = [];
-    while (randoms.length < 3) {
-      var r = Math.floor(Math.random() * cards.cards.length - 1) + 1;
-      if (randoms.indexOf(r) === -1) randoms.push(r);
-    }
-    return randoms;
-  }
-
-  const generateQuestion = () => {
-    setChecked(-1);
-
-    const lista = [];
-    const cartas = cards.cards;
-    if (cards.cards) {
-      if (cartas.length < 4) {
-        hideQuestion();
-        alert("Necesitas agregar almenos 4 cartas para generar un Quiz");
-      } else {
-        const randomsGenerated = differentRandom();
-        const cardChoosen = cartas[randomsGenerated[0]];
-        lista.push(cardChoosen);
-        setCardPicked(cardChoosen);
-        const answerOne = cartas[randomsGenerated[1]];
-        lista.push(answerOne);
-        const answerTwo = cartas[randomsGenerated[2]];
-        lista.push(answerTwo);
-        setAnswers(lista.sort(() => Math.random() - 0.5));
-      }
-    } else {
-      alert("Agrega más cartas 4 cartas para generar un Quiz");
-    }
-  };
-
-  const handleChangeRadio = (e) => {
-    setAnswerSelected(e.target.value);
-    console.log(e.target.id);
-    setChecked(e.target.id);
-  };
-  const handleAnswer = () => {
-    setIsSent(true);
-    // setChecked(-1);
-    if (answerSelected === cardPicked.cardMeaning) {
-      console.log("correct");
-      setResult(true);
-      helpHttp().post(urlIncreaseScore);
-    } else {
-      console.log("incorrect");
-      setResult(false);
-    }
-  };
-
-  const openModalQuestion = (e) => {
-    // e.preventDefault();
-    if (!cards.cards) {
-      alert("Agrega más cartas UwU");
-      return;
-    } else {
-      generateQuestion();
-      modalQuestion ? setModalQuestion(false) : setModalQuestion(true);
-    }
-  };
-  const nextQuestion = () => {
-    setAnswerSelected();
-    generateQuestion();
-    setResult();
-    setIsSent(false);
-  };
-
-  const toggleMode = () => {
-    setMode(!mode);
-  };
+  const openModal = () => setModalIsOpen(!modalIsOpen);
 
   const handleAudio = (sound) => {
     const audio = `data:audio/mpeg;base64,${sound}`;
@@ -197,9 +73,7 @@ const Cards = () => {
   return (
     <>
       <NewMenu />
-
       {/* <AboutUser wasUp={result}></AboutUser> */}
-
       <div className="all-cards">
         {paramsUrl.section === "mis-cartas" && (
           <div className="del-cards">
@@ -291,131 +165,27 @@ const Cards = () => {
             )}
           </div>
         )}
-        {cards.cards && cards.cards.length > 3 && (
-          <Modal
-            // className="modal-card-question"
-            ariaHideApp={false}
-            style={customStyles}
-            isOpen={modalQuestion}
-            contentLabel="Minimal Modal Example"
-            // className="bg-red-400"
-            // className="modal-quiz"
-          >
-            <OutsideClickHandler
-              onOutsideClick={() => {
-                hideQuestion();
-              }}
-            >
-              <label className="switch">
-                <input type="checkbox" onClick={toggleMode} />
-                <span className="slider"></span>
-              </label>
-              {cardPicked && (
-                <>
-                  <div
-                    style={{ color: mode ? "black" : "white" }}
-                    className="container-question-card"
-                  >
-                    <div className="question-card">
-                      Elige el significado de
-                      <strong> {cardPicked.cardTitle}</strong>
-                    </div>
-                    <div className="parent-answers">
-                      {answers &&
-                        answers.map((answer, key) => {
-                          return (
-                            <div key={answer.id} className="box-answers">
-                              <label
-                                className={
-                                  cardPicked.cardMeaning ===
-                                    answer.cardMeaning &&
-                                  isSent &&
-                                  !result
-                                    ? "red-label showAnswer"
-                                    : "rad-label"
-                                }
-                              >
-                                <input
-                                  id={key}
-                                  type="radio"
-                                  className="rad-input"
-                                  name="rad"
-                                  onChange={handleChangeRadio}
-                                  value={answer.cardMeaning}
-                                  checked={checked == key && true}
-                                />
-                                <div className="rad-design"></div>
-                                <div className="rad-text">
-                                  {answer.cardMeaning}
-                                </div>
-                              </label>
-                            </div>
-                          );
-                        })}
-                    </div>
 
-                    <div className="parent-btn">
-                      <button
-                        className={isSent ? "hide" : "btn-send-answer "}
-                        onClick={handleAnswer}
-                      >
-                        Enviar
-                      </button>
-                    </div>
-
-                    <div
-                      className="next-quest"
-                      style={{
-                        bottom: isSent ? "0" : "-83px",
-                      }}
-                      onClick={nextQuestion}
-                    >
-                      <img
-                        onClick={nextQuestion}
-                        className="next-icon"
-                        src={next}
-                        alt=""
-                      />
-                      <div className="container-result-message">
-                        <div
-                          className={
-                            isSent
-                              ? result
-                                ? "message-question answer-right"
-                                : "message-question answer-wrong "
-                              : "hide"
-                          }
-                        >
-                          {result ? "Correcto" : "Incorrecto"}
-                        </div>
-                      </div>
-                      {/* <div className="bg-white">Incorrecto</div> */}
-                    </div>
-                    <div
-                      className={
-                        result
-                          ? "upScore-indicator upScoreAnimation"
-                          : "upScore-indicator"
-                      }
-                    >
-                      <strong>+1XP</strong>
-                    </div>
-                  </div>
-                </>
-              )}
-            </OutsideClickHandler>
-          </Modal>
-        )}
         {isPremium && (
           <div className="cont-btn-review flex justify-center flex-col gap-5 items-center">
             <button
-              className="css-button-shadow-border-sliding--sky "
-              onClick={openModalQuestion}
+              onClick={() =>
+                navigate("/test", {
+                  state: {
+                    cards: cards.cards,
+                    section:
+                      paramsUrl.section === "mis-cartas"
+                        ? "Mis cartas"
+                        : paramsUrl.section,
+                  },
+                })
+              }
+              className="btn bg-blue-700 w-36"
             >
               Quiz
             </button>
-            <Link to="/phrases" state={{ crts: cards.cards }}>
-              {/* <button
+            {/* <Link to="/phrases" state={{ crts: cards.cards }}>
+              <button
                 className="btn btn-secondary"
                 onClick={() => {
                   console.log(cards.cards);
@@ -423,8 +193,8 @@ const Cards = () => {
                 }}
               >
                 HardCore quiz
-              </button> */}
-            </Link>
+              </button>
+            </Link> */}
           </div>
         )}
       </div>
--- src/router/Routers.js
+++ src/router/Routers.js
@@ -1,23 +1,17 @@
 import { Routes, Route } from "react-router-dom";
 import Cards from "../components/Cards";
-// import LoginAuth from "../components/LoginAuth";
-// import LoginPage from "../components/LoginPage";
-// import Session from "../components/Session";
 import { AuthProvider } from "../context/AuthContext";
-
 import PrivateRoute from "./PrivateRoute";
 import Posts from "../components/Posts";
 import PostPage from "../components/PostPage";
 // import Stripe from "../components/Stripe";
 import DeleteCards from "../components/DeleteCards";
-import MenuV2 from "../components/MenuV2";
 import SubMenuCard from "../components/SubMenuCard";
 import Phrases from "../components/Phrases";
 import NewLogin from "../components/NewLogin";
 import NewSignUp from "../components/NewSignUp";
-import NewLeaderBoard from "../components/NewLeaderBoard";
-import NewMenu from "../components/NewMenu";
-import UserInfo from "../components/UserInfo";
+
+import Quiz from "../components/Quiz";
 const Routers = () => {
   return (
     <AuthProvider>
@@ -39,7 +33,7 @@ const Routers = () => {
 
           {/* <Route path="/stripe" element={<Stripe />}></Route> */}
           <Route path="/" element={<SubMenuCard />} />
-          <Route path="/test" element={<UserInfo />}></Route>
+          <Route path="/test" element={<Quiz />}></Route>
         </Route>
       </Routes>
     </AuthProvider>
